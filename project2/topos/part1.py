36441154e981a185367230dbb78134126ac7d1e1
None <None>
2017-04-29 15:32:39 -0700

    haha

--- project2/topos/part1.py
+++ project2/topos/part1.py
@@ -12,6 +12,15 @@ class part1_topo(Topo):
         #switch1 = self.addSwitch('switchname')
         #host1 = self.addHost('hostname')
         #self.addLink(hostname,switchname)
+	h1 = self.addHost('h1')
+	h2 = self.addHost('h2')
+	h3 = self.addHost('h3')
+	h4 = self.addHost('h4')
+	s1 = self.addSwitch('s1')
+	self.addLink(h1, s1)
+	self.addLink(h2, s1)
+	self.addLink(h3, s1)
+	self.addLink(h4, s1)
 
 topos = {'part1' : part1_topo}
 
